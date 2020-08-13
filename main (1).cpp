69ca693d3708f6ca0e8a955da13d80116f9ca465
DanielTalavera <69166077+DanielTalavera@users.noreply.github.com>
2020-08-13 10:02:14 -0400

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+int main()
+{
+int a,b,c;
+cout<<"Ingresa el primer operador: ";
+cin>>a;
+cout<<"Ingresa el segundo operando: ";
+cin>>b;
+c=a+b;
+cout<<"La suma es: "<<c;
+return 0; 
+}
